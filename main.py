0b2a042fe53e7f953b42142a7b8b242c87c0912c
Albert Alises <albert.alises@gmail.com>
2018-11-05 17:08:10 +0100

    Extracted config into separate file. Added prefix /api/v1 to app

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from flask import Flask, redirect
+from flask import Flask, Blueprint, redirect
 from flask_restful import Api
 from sqlalchemy_utils import database_exists
-
+from config import APP_CONFIG, API_PREFIX, DB_NAME
 from api.resources.civilization import Civilization, CivilizationList
 from api.resources.unit import Unit, UnitList
 from api.resources.structure import Structure, StructureList
@@ -9,19 +9,25 @@ from api.resources.technology import Technology, TechnologyList
 from db.populate_tables import populate_db
 from db.db import db
 
-DB_NAME = 'sqlite:///db/data.db'
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = DB_NAME
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['PROPAGATE_EXCEPTIONS'] = True
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
-app.config['JSON_SORT_KEYS'] = False
+app.config.update(APP_CONFIG)
 
-api = Api(app)
+api_blueprint = Blueprint('api', __name__)
+api = Api(api_blueprint, prefix=API_PREFIX)
 
+# Routes of our application
+api.add_resource(Civilization, '/civilization/<string:_id>')
+api.add_resource(CivilizationList, '/civilizations')
+api.add_resource(Unit, '/unit/<string:_id>')
+api.add_resource(UnitList, '/units')
+api.add_resource(Structure, '/structure/<string:_id>')
+api.add_resource(StructureList, '/structures')
+api.add_resource(Technology, '/technology/<string:_id>')
+api.add_resource(TechnologyList, '/technologies')
+
+app.register_blueprint(api_blueprint)
 db.init_app(app)
-
 
 @app.before_first_request
 def create_tables():
@@ -29,23 +35,10 @@ def create_tables():
         db.create_all()
         populate_db()
 
-
-api.add_resource(Civilization, '/civilization/<string:_id>')
-api.add_resource(CivilizationList, '/civilizations')
-
-api.add_resource(Unit, '/unit/<string:_id>')
-api.add_resource(UnitList, '/units')
-
-api.add_resource(Structure, '/structure/<string:_id>')
-api.add_resource(StructureList, '/structures')
-
-api.add_resource(Technology, '/technology/<string:_id>')
-api.add_resource(TechnologyList, '/technologies')
-
-
-@app.route('/')
+@app.route(API_PREFIX)
+@app.route("/")
 def redirect_to_civilizations():
-    return redirect("/civilizations", code=302)
+    return redirect("{}/civilizations".format(API_PREFIX), code=302)
 
 
 if __name__ == "__main__":
