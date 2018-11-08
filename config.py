673f06e9c44690d1669b22b3af84cd95f8c827bc
Albert Alises <albert.alises@gmail.com>
2018-11-08 10:30:48 +0100

    Add Flasgger for docs generation. now '/' redirects to 'docs'

--- config.py
+++ config.py
@@ -8,5 +8,25 @@ APP_CONFIG = {
     'SQLALCHEMY_TRACK_MODIFICATIONS': False,
     'PROPAGATE_EXCEPTIONS': True,
     'JSONIFY_PRETTYPRINT_REGULAR': True,
-    'JSON_SORT_KEYS': False
+    'JSON_SORT_KEYS': False,
+    'SWAGGER': {
+        'title': 'Age of Empires 2 API',
+        'uiversion': 3
+    }
+}
+
+SWAGGER_CONFIG = {
+    "headers": [
+    ],
+    "specs": [
+        {
+            "endpoint": 'apispec',
+            "route": '/apispec.json',
+            "rule_filter": lambda rule: True,
+            "model_filter": lambda tag: True
+        }
+    ],
+    "static_url_path": "/flasgger_static",
+    "swagger_ui": True,
+    "specs_route": "/docs/"
 }
